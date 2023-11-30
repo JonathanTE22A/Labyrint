211333bc7c5b39f1e49191107513112fa24b0025
Theozzzzzz <theo.strindmark@gmail.com>
2023-11-30 13:44:10 +0100

    Början av Vapen

---
+++ Vapen.py
@@ -0,0 +1,35 @@
+import random
+
+class Item:
+    def __init__(self,weapon_type, damage, critchanse):
+        self.weapon_type = weapon_type
+        self.damage = damage
+        self.critchanse = critchanse
+
+    def presentweapon(self):
+        print("Du hittade " + self.weapon_type + " som gör "  + self.damage+ " skada och har en critchanse på " + self.critchanse)
+exp = ""
+
+
+def Level_system()
+if monster_hp() < player_hp():
+    level_up()
+    
+
+def level_up():
+    if exp == 1:
+        list = [Weapon1,Weapon2,Weapon3,Weapon4]
+        print(random(list))
+
+
+
+Weapon1 = Item("Pilbåge", "15", "32%")
+Weapon2 = Item("Svärd", "20", "10%")
+Weapon3 = Item("Lans", "25", "5%")
+Weapon4 = Item("Pinne", "8", "50%")
+
+Weapon1.presentweapon()
+Weapon2.presentweapon()
+Weapon3.presentweapon()
+Weapon4.presentweapon()
+
